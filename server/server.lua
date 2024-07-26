1fa57c99162fd3d4edc9e23acabf016196a105eb
Ekali <pcnlmmm@gmail.com>
2024-07-26 23:34:00 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,11 +1,9 @@
-ESX = exports["es_extended"]:getSharedObject()
 local prefix = ("%s:"):format(GetCurrentResourceName())
 
-ESX.RegisterServerCallback(prefix.."checkPermission", function(source, cb)
-    local perm = ESX.GetPlayerFromId(source).getGroup()
-    local allowed = false
-    for _,v in pairs(Config.perms) do
-        if (perm == v) then allowed = true end
+lib.addCommand(Config.CommandName, {restricted = Config.AceAllowed}, function(source)
+    if not DoesPlayerExist(source) then
+       return
     end
-    cb(allowed, perm)
+
+    TriggerClientEvent(("%sopenCoordsMenu"):format(prefix), source)
 end)
